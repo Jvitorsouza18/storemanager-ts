2144589000efe81d50425dea8f83ab2df8a78adb
Joao Vitor <joao@Joo.localdomain>
2022-06-15 14:27:50 -0300

    Starting project

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+require('dotenv').config();
+
+const app = express();
+
+// não remova esse endpoint, e para o avaliador funcionar
+app.get('/', (_request, response) => {
+  response.send();
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Escutando na porta ${process.env.PORT}`);
+});
