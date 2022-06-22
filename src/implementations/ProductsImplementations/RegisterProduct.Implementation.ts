eea25bd7c67467ef65319810916c23fb3839927f
João Vitor Souza Rocha <jvitorsouza18@gmail.com>
2022-06-22 16:49:10 -0300

    feat: product register validation

--- src/implementations/ProductsImplementations/RegisterProduct.Implementation.ts
+++ src/implementations/ProductsImplementations/RegisterProduct.Implementation.ts
@@ -1,12 +1,20 @@
-import { ResultSetHeader } from 'mysql2';
+import { ResultSetHeader, RowDataPacket } from 'mysql2';
 
 import { IProduct } from '../../entities/IProductsEntitiy';
 import connection from '../../helpers/connection';
 import { IRegisterProductRepository } from '../../repositories/ProductsRepository/IRegisterProductRepository';
+import { CustomError } from '../../utils/CutomError';
 
 export class RegisterProductImplementation implements IRegisterProductRepository {
   async registerProduct(product: IProduct): Promise<IProduct> {
     const query = 'INSERT INTO StoreManager.products (name, quantity) VALUES (?, ?)';
+    const validationQuery = 'SELECT * from StoreManager.products WHERE name = ?';
+    const [productAlreadyExists] = await connection.execute<RowDataPacket[]>(
+      validationQuery,
+      [product.name],
+    );
+    if (productAlreadyExists.length) throw new CustomError(409, 'Product already exists');
+
     const [result] = await connection.execute<ResultSetHeader>(query, [
       product.name,
       product.quantity,
